f967be9c79e69956db286d1d7c21610263002933
KUDIPUDIALEKHYA <alekhyak086@gmail.com>
2026-04-20 23:30:24 +0530

    practise

--- day11-Lists/DuplicatesUsingList.py
+++ day11-Lists/DuplicatesUsingList.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a = list(map(int, input("Enter list 1: ").split()))
 b = list(map(int, input("Enter list 2: ").split()))
 result = a.copy()
@@ -6,12 +5,3 @@ for i in b:
     if i not in result:
         result.append(i)
 print("Merged list:", result)
-=======
-a = list(map(int, input("Enter list 1: ").split()))
-b = list(map(int, input("Enter list 2: ").split()))
-result = a.copy()
-for i in b:
-    if i not in result:
-        result.append(i)
-print("Merged list:", result)
->>>>>>> 26d71c6 (Updated list programs)
